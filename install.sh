c7a9a527cff0f9440b4e6208fe7d2ddee0be09d4
datprs <datprs2k1@gmail.com>
2025-03-01 09:06:55 +0700

    Update

--- install.sh
+++ install.sh
@@ -155,7 +155,7 @@ install_XrayR() {
     chmod +x XrayR
     mkdir /etc/XrayR/ -p
     rm /etc/systemd/system/XrayR.service -f
-    file="https://github.com/DauDau432/XrayR-release/raw/main/XrayR.service"
+    file="https://raw.githubusercontent.com/datprs2k1/XrayR-Script/main/XrayR.service"
     wget -N --no-check-certificate -O /etc/systemd/system/XrayR.service ${file}
     #cp -f XrayR.service /etc/systemd/system/
     systemctl daemon-reload
